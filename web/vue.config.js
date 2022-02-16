b3699fc7bb252d4a4c4062f06661414026ab484c
YOUJUN <824000803@qq.com>
2022-02-16 15:11:05 +0800

    fix: remove image-webpack-loader 7.0.0; casing alias bug

--- web/vue.config.js
+++ web/vue.config.js
@@ -159,18 +159,6 @@ module.exports = function(){
                     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
             );
 
-            config.module
-                .rule("images")
-                .use("image-webpack-loader")
-                .loader("image-webpack-loader")
-                .options({
-                    mozjpeg: { progressive: true, quality: 65 },
-                    optipng: { enabled: false },
-                    pngquant: { quality: [0.65, 0.9], speed: 4 },
-                    gifsicle: { interlaced: false },
-                    webp: { quality: 75 }
-                });
-
             //优化moment 去掉国际化内容；
             //我都不知道哪来的moment
             config
